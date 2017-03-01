326efead245001d19b030eba6e964ffbc7808ee0
xfree86 <woshixjr@gmail.com>
2017-03-01 17:38:02 +0800

    运算符

---
+++ code/ex3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+print "I will now count my chickens:"
+print u"我现在数我的鸡："
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print u"3 + 2 < 5 - 7 这是真的吗？"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print u"3 + 2 是什么？", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print u"5 - 7 是什么？", 5 - 7
+
+print "Oh, that's why it's False."
+print u"噢，这就是为什么它是假的。"
+
+print "How about some more."
+print u"再来一些怎么样。"
+
+print "Is it greater?", 5 > -2
+print u"它更大吗？", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print u"它更大还是相等？", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print u"它更小还是相等？", 5 <= -2
