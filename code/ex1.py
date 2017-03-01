b896bb0ab344b9e273c93d9b38ce07e25ad6a032
xfree86 <woshixjr@gmail.com>
2017-03-01 16:08:27 +0800

    print和注释

---
+++ code/ex1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print u"你好！"
+print u"再一次你好！"
+print u"我喜欢按这个。"
+print u'耶！ 打印中。'
+print u"我宁愿你‘不’。"
+print u'我 “说过” 不要触摸这个。'
---
+++ code/ex2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# A comment, this is so you can read your program later.
+# 一个注释，这是为了你以后可以读你的代码。
+# Anything after the # is ignored by python.
+# python中任何东西在#后面是无效的。
+
+print "I could have code like this."  # and the comment after is ignored
+
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# 你也可以使用一个注释去“关闭”或者注释掉一段代码：
+# print "This won't run."
+# print u"这个不会运行。"
+
+print "This will run."
+print u"这个将会运行。"
