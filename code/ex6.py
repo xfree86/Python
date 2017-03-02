e9cd94f33a3bbb227e976d5c72a6a9fbcf61597b
xfree86 <woshixjr@gmail.com>
2017-03-02 17:40:50 +0800

    更多打印、字符串、变量、格式化字符、运算符练习

--- code/ex6.py
+++ code/ex6.py
@@ -2,16 +2,16 @@
 
 # 大量的字符串、变量、简写的变量和格式化字符
 
-# 将变量x赋值为字符串，%d格式化字符调用后面的数字10
+# 给变量x赋值字符串，%d格式化字符调用后面的数字10
 x = "There are %d types of people." % 10
 
-# 将变量binary赋值为字符串
+# 给变量binary赋值字符串
 binary = "binary"
 
-# 将变量do_not赋值为字符串
+# 给变量do_not赋值字符串
 do_not = "don't"
 
-# 将变量y赋值为字符串，两个%s格式化字符调用后面的变量binary和变量do_not
+# 给变量y赋值字符串，两个%s格式化字符调用后面的变量binary和变量do_not
 # 字符串包含字符串
 y = "Those who know %s and those who %s." % (binary, do_not)
 
@@ -29,20 +29,20 @@ print "I said: %r." % x
 # 字符串包含字符串
 print "I also said: '%s'." % y
 
-# 将变量hilarious赋值为布尔值False(假)
+# 给变量hilarious赋值布尔值False(假)
 hilarious = False
 
-# 将变量joke_evaluation赋值为字符串，%r是格式化字符，但该语句后未跟随%符号和被调用值
+# 给变量joke_evaluation赋值字符串，%r是格式化字符，但该语句后未跟随%符号和被调用值
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 # 打印变量joke_evaluation的值，但在打印前传送被调用值（这里是hilarious变量）给变量joke_evaluation中的%r格式化字符
 # 字符串包含字符串
 print joke_evaluation % hilarious
 
-# 将变量w赋值为字符串
+# 给变量w赋值字符串
 w = "This is the left side of..."
 
-# 将变量e赋值为字符串
+# 给变量e赋值字符串
 e = "a string with a right side."
 
 # 打印变量w和e合并后的值
---
+++ code/ex7.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# 字符串、变量、格式化符号、运算符练习
+
+# 打印字符串
+print "Mary had a little lamb."
+
+# 打印字符串，%s格式化字符调用后面的snow字符串
+print "Its fleece was white as %s." % 'snow'
+
+# 打印字符串
+print "And everywhere that Mary went."
+
+# 打印字符串，10个点
+print "." * 10 # what'd that do?
+
+# 给变量end1赋值字符串C
+end1 = "C"
+
+# 给变量end2赋值字符串h
+end2 = "h"
+
+# 给变量end3赋值字符串e
+end3 = "e"
+
+# 给变量end4赋值字符串e
+end4 = "e"
+
+# 给变量end5赋值字符串s
+end5 = "s"
+
+# 给变量end6赋值字符串e
+end6 = "e"
+
+# 给变量end7赋值字符串B
+end7 = "B"
+
+# 给变量end8赋值字符串u
+end8 = "u"
+
+# 给变量end10赋值字符串r
+end9 = "r"
+
+# 给变量end10赋值字符串g
+end10 = "g"
+
+# 给变量end11赋值字符串e
+end11 = "e"
+
+# 给变量end12赋值字符串r
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+# 逗号表示在同一行输出
+# 打印变量end1、end2、end3、end4、end5和end6合并后的值，并在同一行输出的print语句的值，两个值用一个空格隔开。
+print end1 + end2 + end3 + end4 + end5 + end6,
+
+#打印变量end7、end8、end9、end10、end11和end12合并后的值
+print end7 + end8 + end9 + end10 + end11 + end12
